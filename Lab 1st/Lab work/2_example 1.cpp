87fc5f28fb8684d63304e5ffc25bb2cd3c97c971
muhammadsuleman13790-dot <muhammadsuleman13790@gmail.com>
2026-03-08 11:08:42 +0500

    Add files via upload

---
+++ Lab 1st/Lab work/2_example 1.cpp
@@ -0,0 +1,11 @@
+     // Fig. 1.2: fig01_02.cpp
+    // A first program in C++.
+#include <iostream>
+using namespace std;
+// function main begins program execution
+int main()
+    {
+    cout << "Welcome to C++!\n";                                    
+    return 0;   // indicate that program ended successfully
+    } // end function main
+
---
+++ Lab 1st/Lab work/5_6_Data type and Variable.cpp
@@ -0,0 +1,23 @@
+     // Fig. 1.2: fig01_02.cpp
+    // A first program in C++.
+#include <iostream>
+using namespace std;
+// function main begins program execution
+int main()
+    {
+    float a = 10.2, b, c;
+    
+    cout<< "Enter value for a:\n";
+    cin >> a;
+    
+    cout<< "Enter value for b:\n";
+    cin >> b;
+    
+    c = a+b;
+    
+    cout << "a + b = " << c;
+    
+    
+    return 0;   // indicate that program ended successfully
+    } // end function main
+
