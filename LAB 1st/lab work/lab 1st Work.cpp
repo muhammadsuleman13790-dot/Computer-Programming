f2472c60f157bc6687ac9f7d1c9d05e6cd41d22a
muhammadsuleman13790-dot <muhammadsuleman13790@gmail.com>
2026-04-21 19:33:16 +0500

    Update lab 1st Work.cpp

--- LAB 1st/lab work/lab 1st Work.cpp
+++ LAB 1st/lab work/lab 1st Work.cpp
@@ -1,11 +1,13 @@
-#include <iostream>   // Library used for input and output operations
-using namespace std;  // Allows us to use standard library names without std::
 
-/*
     Name: Muhammad Suleman
     Registration Number: 5123445
     Department: Electrical Power
     Section: A
+    
+    #include <iostream>   // Library used for input and output operations
+using namespace std;  // Allows us to use standard library names without std::
+
+/*
 
     Program: Simple Hello World Program
     Description: This program prints "Hello World!" on the screen.
