567383a87cc1feca56b9cdd6298daa8f3cc4408d
muhammadsuleman13790-dot <muhammadsuleman13790@gmail.com>
2026-05-19 07:28:38 +0500

    Add files via upload

---
+++ Lab 6th/Lab Tasks/Lab 6 Task 1st.cpp
@@ -0,0 +1,17 @@
+// Name: Muhammad Suleman 
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0759
+// Section: A
+
+
+#include <iostream> //tells the compiler to include input output stream librry
+using namespace std; // tells the compiler to use standard namespace std:: for each cin and cout 
+                     // without having need to write separately
+
+int main() {          // main functions start and body of main function
+	
+	for(int a=1; a<=10; a++)  // for loop with initialization and increment loop runs 10 times
+	cout<<a<<endl;        // output sent to the dispaly
+	
+	return 0;   // tells the compiler that program has ended successfully
+}   // closing braces
