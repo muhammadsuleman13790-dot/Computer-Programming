1074c3ae6d268b485b23d5b92e7d4d47195b06f9
muhammadsuleman13790-dot <muhammadsuleman13790@gmail.com>
2026-06-30 01:46:42 +0500

    Add files via upload

---
+++ LAB 09/Lab work/lab 9 Lab work 2nd.cpp
@@ -0,0 +1,22 @@
+// Name: Muhammad Suleman 
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0759
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void sayHello()
+{
+    // Display message
+    cout << "Hello, World!" << endl;
+}
+
+int main()
+{
+    // Function call
+    sayHello();
+
+    return 0;
+}
