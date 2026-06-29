a458bfd38d0b00c5dd28e9912dcea947034b963f
muhammadsuleman13790-dot <muhammadsuleman13790@gmail.com>
2026-06-30 02:01:46 +0500

    Add files via upload

---
+++ LAB 13/Lab work/lab 13 Lab work 1.cpp
@@ -0,0 +1,26 @@
+// Name: Muhammad Suleman 
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0759
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+// Declaring structure Person
+struct Person {
+    string name;
+    int age;
+    string address;
+};
+
+int main() {
+    // Declaring and initializing structure variable person1
+    Person person1 = {"Muhammad Suleman", 19, "500kv GRID STATION NGC Peshawar, Khyber Pakhtunkhwa"};
+
+    // Displaying structure members using dot notation
+    cout << "Name: " << person1.name << endl;
+    cout << "Age: " << person1.age << endl;
+    cout << "Address: " << person1.address << endl;
+
+    return 0;
+}
---
+++ LAB 13/Lab work/lab 13 Lab work 2.cpp
@@ -0,0 +1,26 @@
+// Name: Muhammad Suleman 
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0759
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+// Declaring structure Student
+struct Student {
+    string name;
+    int age;
+    char grade;
+};
+
+int main() {
+    // Declaring and initializing structure variable student1
+    Student student1 = {"Muhammad suleman", 25, 'A'};
+
+    // Displaying structure members
+    cout << "Name: " << student1.name << endl;
+    cout << "Age: " << student1.age << endl;
+    cout << "Grade: " << student1.grade << endl;
+
+    return 0;
+}
